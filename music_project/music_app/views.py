4419f179cc31cf1cc9245ffd62c18a54aab2fa3b
Mr-Nelson <kdnels88@gmail.com>
2021-07-09 14:30:43 -0500

    mvp for music library Django REST framework project

--- music_project/music_app/views.py
+++ music_project/music_app/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework.views import APIView
@@ -19,3 +21,36 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+
+    def get_by_id(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        song_id = self.get_by_id(pk)
+        serializer = SongSerializer(song_id)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        song_id = self.get_by_id(pk)
+        serializer = SongSerializer(song_id, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        song_id = self.get_by_id(pk)
+        serializer = SongSerializer(song_id, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+
+    def delete(self, request, pk):
+        song_id = self.get_by_id(pk)
+        deletedSong = SongSerializer(song_id)
+        song_id.delete()
+        return Response(deletedSong.data, status=status.HTTP_200_OK)
